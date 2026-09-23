2e6e670cde7440274f8ce9e016099d27e81b315d
JMontes <damon.1986@icloud.com>
2026-09-23 10:34:12 -0500

    feat: add projection login page

---
+++ src/app/projection/login/page.tsx
@@ -0,0 +1,9 @@
+import { ProjectionLoginForm } from "@/components/auth/projection-login-form";
+
+export default function ProjectionLoginPage() {
+  return (
+    <div className="container mx-auto flex items-center justify-center px-4 py-10 md:py-20">
+      <ProjectionLoginForm />
+    </div>
+  );
+}
